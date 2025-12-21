b056e86a08b9c95393a0e26ebeee39875d6e0cd5
Jannled <7737131+Jannled@users.noreply.github.com>
2025-12-21 03:13:46 +0100

    First Prototype

---
+++ DoorstopMCP.py
@@ -0,0 +1,74 @@
+import logging
+from typing import cast
+
+import doorstop
+from doorstop import Document, DoorstopError, Tree
+from mcp.server.fastmcp import FastMCP
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+doorstop_root = 'docs'
+
+mcp = FastMCP(
+    'DoorstopMCP',
+    json_response=True,
+    debug=True,
+    host='0.0.0.0',
+    port=3001
+)
+
+@mcp.resource('doorstop://version')
+def get_doorstop_version():
+    """Get the version of the underlying Doorstop Framework"""
+
+    return doorstop.__version__
+
+
+@mcp.resource('doorstop://list_documents')
+def list_documents() -> list[Tree]:
+    """
+    List all defined documents
+    
+    :return: A list of documents
+    """
+
+    tree = get_tree()
+    return cast(Tree, tree.documents)
+
+
+@mcp.resource('doorstop://find_document/{prefix}')
+def find_document(prefix: str) -> Document:
+    """
+    Docstring for find_document
+    
+    :param prefix: Description
+    """
+
+    tree = get_tree()
+    document = tree.find_document(prefix)
+
+    return document
+
+
+def get_tree():
+    tree = doorstop.build(root=doorstop_root)
+    return tree
+
+
+# Run with streamable HTTP transport
+if __name__ == "__main__":
+
+    try:
+        # Dry run to check if Doorstop dir is healthy
+        get_tree()
+
+        # Start the MCP Server
+        mcp.run(transport="streamable-http")
+    
+    except DoorstopError as e:
+        logging.error(e)
+        logging.error('This might indicate that no version control system is configured for this directory.')
+
+    except KeyboardInterrupt:
+        logging.info('Keyboard interrupt, exiting...')
---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Doorstop MCP
+Quick and dirty implementation of a [MCP Server](https://github.com/modelcontextprotocol/python-sdk) for the Python [Doorstop framework](https://github.com/doorstop-dev/doorstop).
+
+## Testing the API
+You can explore the MCP by using the following web GUI:
+
+```bash
+npx -y @modelcontextprotocol/inspector
+```
+
+Options:
+- Transport Type: Streamable HTTP
+- URL: [localhost:3001/mcp](http://localhost:3001/mcp)
+- Connection Type: Via Proxy
